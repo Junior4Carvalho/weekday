a0f451fcaf8d79d1c7f70fa4e37c4867c291e90d
Junior Carvalho <100240370+Junior4Carvalho@users.noreply.github.com>
2022-07-29 01:29:50 +0100

    Create week.py
    first commit

---
+++ Script/week.py
@@ -0,0 +1,17 @@
+week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+ask = input("What day is today? ")
+if ask.capitalize() in week:
+    try:
+        if ask.capitalize() != week[0] and ask.capitalize() != week[6]:
+            print("Today is a Weekday!")
+
+        else:
+            print("Today is a Weekend!")
+
+    except:
+        print("Type only what's on this list: \n {} ".format(week))
+
+else:
+    print("Type only what's on this list: \n {} ".format(week))
+    
+#Copyright__@Junior_Carvalho06
